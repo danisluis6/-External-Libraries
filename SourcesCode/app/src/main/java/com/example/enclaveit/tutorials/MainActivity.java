95b7eaaa0f994982fa12ab279007acff9220163e
danisluis6 <vuongluis@hotmail.com>
2016-12-08 16:23:07 +0700

    ok

--- SourcesCode/app/src/main/java/com/example/enclaveit/tutorials/MainActivity.java
+++ SourcesCode/app/src/main/java/com/example/enclaveit/tutorials/MainActivity.java
@@ -1,24 +1,13 @@
 package com.example.enclaveit.tutorials;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.enclaveit.tutorials.aboutactivity.Level0Activity;
-
-import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level0);
-        try {
-            sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        this.startActivity(new Intent(MainActivity.this,Level0Activity.class));
+        setContentView(R.layout.activity_main);
     }
 }
